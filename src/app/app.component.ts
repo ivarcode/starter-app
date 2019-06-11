920c39a21de5576ff95563177cc7342c7d86c2da
Camden Wagner <camden.i.wagner@ivarcode.net>
2019-06-11 15:53:50 -0400

    games (list component pulled out of the main app component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GamesService } from './games.service';
-import { Game } from './models/game.interface';
 
 @Component({
   selector: 'app-root',
@@ -8,22 +6,7 @@ import { Game } from './models/game.interface';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  games: Game[];
   title = 'starter-app';
-  constructor(private gamesService: GamesService) {}
-  ngOnInit() {
-    this.games = this.gamesService.getGames();
-  }
-  addGame(): void {
-    this.games.push({
-      id: this.games.length + 1,
-      white: 'cam',
-      black: 'nick',
-      numberOfMoves: 75,
-      result: 'black wins by resignation in a drawn position'
-    });
-  }
-  deleteGame(gid: number): void {
-    this.games.splice(gid, 1);
-  }
+  constructor() {}
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -8,10 +9,11 @@ import { GamesService } from './games.service';
 
 // gameview component
 import { GameviewComponent } from './gameview/gameview.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
-  declarations: [AppComponent, GameviewComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, GameviewComponent, GamesComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [GamesService],
   bootstrap: [AppComponent]
 })
